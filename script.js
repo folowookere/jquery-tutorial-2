583554c3f596367b181ab8870c70dab18c23bf5a
Funmi <folowookere@yahoo.com>
2018-05-30 14:08:53 -0400

    Lesson 3

--- script.js
+++ script.js
@@ -3,27 +3,29 @@
 $(function(){
     
     //jquery Events
-    // $("p").on("click mouseenter", function(){
-    //     console.log("You clicked a paragraph");
-    // });
-    
-     $("p").one({
-        "click": function() { console.log ("Clicked"); },
-        "mouseover": function() { console.log("Hovered"); }
+ //  $("h1").hide(500); // <-- can pass in a number (ms) or slow/fast
+ //  $("h1").show(2000); //combination of slide and fade effects
+   
+  //  $("img").slideUp(500); // <-- can pass in a number (ms) or slow/fast
+  //  $("img").slideDown(2000); 
+   
+    // $("img").fadeOut(500); // <-- can pass in a number (ms) or slow/fast
+  //   $("img").fadeIn("normal"); 
+     
+  //   $("h1").toggle(900); // <-- can pass in a number (ms) or slow/fast
+  //   $("h1").toggle("normal"); //can also do fade toggle or slide toggle to do opposite of what current state is
+
+    $("h1").hide();
+    $("h1").fadeIn(2000, function(){
+        
+    $(this).addClass("blue");
     });
     
-    $("p").off("click");
-    
-    // $("p").hover(function() {
-    //   console.log("You entered.");
-    // },
-    // function() {
-    //     console.log("You left");
-    // });
-    
-    $("input").change(function() {
-        console.log("You did something");
+    $("img").on("click", function(){
+        $("h1").stop();
     });
+    
+    $("p").hide(500).delay(4000).show(400);
 });
 
 //Types of jquery methods
